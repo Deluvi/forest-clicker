6528df096b2154f500bd0d0fc0a6781b71578490
Deluvi <noe.baylac@protonmail.com>
2019-08-03 19:29:21 +0200

    Working prototype

--- Planet.gd
+++ Planet.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var grass = preload("res://Grass.tscn")
 onready var small_tree = preload("res://SmallTree.tscn")
+onready var bush = preload("res://Bush.tscn")
+
 signal dead
 
 var dying = false
@@ -17,21 +19,26 @@ func get_random_coord_radius():
 	return random_vec*radius
 
 func bouik():
-	$Bouik.stop()
-	$Bouik.play("Bouik")
+	if ! dying:
+		$Bouik.stop()
+		$Bouik.play("Bouik")
 
 func make_veg(scene: PackedScene, coef: float):
-	var new_veg = scene.instance()
-	new_veg.position = get_random_coord_radius() * coef
-	orient_obj_to_planet(new_veg)
-	add_child(new_veg)
-	veg.push_back(new_veg)
+	if ! dying:
+		var new_veg = scene.instance()
+		new_veg.position = get_random_coord_radius() * coef
+		orient_obj_to_planet(new_veg)
+		add_child(new_veg)
+		veg.push_back(new_veg)
 
 func make_grass():
 	make_veg(grass, 1.02)
 
 func make_small_tree():
 	make_veg(small_tree, 1)
+
+func make_bush():
+	make_veg(bush, 1)
 
 func orient_obj_to_planet(obj: Node2D):
 	var angle = (Vector2(0,0).angle_to_point(obj.position)) - 1.570796
@@ -41,9 +48,18 @@ func start_death():
 	$AnimatedSprite.play("death")
 	dying = true
 
+func start_life():
+	$AnimatedSprite.play("reveil")
+	$AnimatedSprite.stop()
+	dying = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start_life()
+
+func progress_life():
+	if ! dying:
+		$AnimatedSprite.frame = $AnimatedSprite.frame + 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -62,6 +78,6 @@ func _on_AnimatedSprite_frame_changed():
 		remove_veg(veg.size()/2)
 
 func remove_veg(amount: int):
-	for i in range(amount):
+	for _i in range(amount):
 		var veg_inst = veg.pop_back()
 		veg_inst.queue_free()
--- World.gd
+++ World.gd
@@ -1,8 +1,10 @@
 extends Node
 
-var forest_score = 0
+var forest_score: float = 0
 
 var tap_number = 0
+
+var last_time_tap = 0
 
 export var forest_threshold = 100
 
@@ -12,6 +14,12 @@ func orient_obj_to_planet(obj: Node2D):
 	var angle = (planet_coord.angle_to_point(obj.position))
 	obj.rotation = angle
 
+func update_score(delta: float):
+	print(delta)
+	var points = (1/(delta/2000))
+	forest_score += points
+	print(forest_score)
+
 func tap():
 	var time = OS.get_ticks_msec()
 	print(time)
@@ -19,8 +27,14 @@ func tap():
 	$Planet.bouik()
 	$Planet.make_grass()
 	$Planet.make_small_tree()
+	$Planet.make_bush()
+	$Planet.progress_life()
 	tap_number = tap_number + 1
-	if tap_number == 10:
+	#if tap_number == 10:
+#		$Planet.start_death()
+	update_score(time - last_time_tap)
+	last_time_tap = time
+	if forest_threshold < forest_score:
 		$Planet.start_death()
 
 # Called when the node enters the scene tree for the first time.
@@ -36,3 +50,12 @@ func _input(event):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_DeathCooldown_timeout():
+	$Planet.start_life()
+	forest_score = 0
+
+
+func _on_Planet_dead():
+	$DeathCooldown.start()
